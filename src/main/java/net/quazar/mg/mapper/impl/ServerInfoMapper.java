6536d961e433d3a8b7289e87c338bb95b633b012
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-07-08 02:34:39 +0300

    Added server info mapper

---
+++ src/main/java/net/quazar/mg/mapper/impl/ServerInfoMapper.java
@@ -0,0 +1,38 @@
+package net.quazar.mg.mapper.impl;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.quazar.mg.mapper.ObjectMapper;
+import net.quazar.mg.model.GameServer;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+public final class ServerInfoMapper implements ObjectMapper<ServerInfo, GameServer> {
+    @Override
+    public GameServer mapTo(ServerInfo source) {
+        InetSocketAddress address = (InetSocketAddress) source.getSocketAddress();
+        return new GameServer
+                (
+                        source.getName(),
+                        address.getHostName() + ":" + address.getPort(),
+                        source.getMotd(),
+                        source.isRestricted(),
+                        null,
+                        source
+                );
+    }
+
+    @Override
+    public ServerInfo mapFrom(GameServer gameServer) {
+        String[] addr = gameServer.getAddress().split(":");
+        SocketAddress socketAddress = InetSocketAddress.createUnresolved(addr[0], Integer.parseInt(addr[1]));
+        return ProxyServer.getInstance().constructServerInfo
+                (
+                        gameServer.getName(),
+                        socketAddress,
+                        gameServer.getMotd(),
+                        gameServer.isRestricted()
+                );
+    }
+}
