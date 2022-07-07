4615d9165b9cc14e5508b7eb3c2d4b79beee80db
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-07-08 02:38:27 +0300

    Sql queries optimization and server types support

--- src/main/java/net/quazar/mg/database/Sql.java
+++ src/main/java/net/quazar/mg/database/Sql.java
@@ -1,20 +1,24 @@
 package net.quazar.mg.database;
 
+import org.intellij.lang.annotations.Language;
+
 public enum Sql {
     CREATE_TABLE("CREATE TABLE IF NOT EXISTS `server_info`(\n" +
             "    `name` VARCHAR(32) NOT NULL PRIMARY KEY,\n" +
             "    `address` VARCHAR(100) NOT NULL,\n" +
             "    `motd` VARCHAR(255) NOT NULL,\n" +
-            "    `restricted` BOOLEAN NOT NULL DEFAULT false\n" +
+            "    `restricted` BOOLEAN NOT NULL DEFAULT false,\n" +
+            "    `type` VARCHAR(32) DEFAULT 'game'\n" +
             ");"),
     SELECT_BY_NAME("SELECT * FROM `server_info` where `name`=?"),
     SELECT_ALL("SELECT * FROM `server_info`"),
-    INSERT("INSERT INTO `server_info`(`name`, `address`, `motd`, `restricted`) VALUES (?, ?, ?, ?)"),
+    INSERT("INSERT INTO `server_info`(`name`, `address`, `motd`, `restricted`, `type`) VALUES (?, ?, ?, ?, ?)" +
+            "ON DUPLICATE KEY UPDATE `address`=?, `motd`=?, `restricted`=?, `type`=?"),
     DELETE("DELETE FROM `server_info` WHERE `name`=?");
 
     private final String query;
 
-    Sql(String query) {
+    Sql(@Language("MySQL") String query) {
         this.query = query;
     }
 
