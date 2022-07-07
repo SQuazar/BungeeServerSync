5cce07e4513d8ed86e7c49b2493ef0395dd4f3c9
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-07-08 02:32:22 +0300

    Added GameServerNotFoundException

---
+++ src/main/java/net/quazar/mg/exception/GameServerNotFoundException.java
@@ -0,0 +1,15 @@
+package net.quazar.mg.exception;
+
+public class GameServerNotFoundException extends ServerInfoException {
+    public GameServerNotFoundException(String message) {
+        super(message);
+    }
+
+    public GameServerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameServerNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
