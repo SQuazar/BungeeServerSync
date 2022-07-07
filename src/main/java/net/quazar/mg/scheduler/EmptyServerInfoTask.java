5da93c8a85b73fffc71ac7cdaced17be297d031e
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-07-08 02:31:34 +0300

    Fixed empty server task

--- src/main/java/net/quazar/mg/scheduler/EmptyServerInfoTask.java
+++ src/main/java/net/quazar/mg/scheduler/EmptyServerInfoTask.java
@@ -5,21 +5,24 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
 import net.quazar.mg.BungeeServerSync;
+import net.quazar.mg.model.GameServer;
 
 import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
-public class EmptyServerInfoTask implements Runnable {
+public final class EmptyServerInfoTask implements Runnable {
 
     private final BungeeServerSync plugin;
     private final ServerInfo serverInfo;
+    private final GameServer gameServer;
     private ScheduledTask scheduledTask;
 
     @Override
     public void run() {
         scheduledTask = ProxyServer.getInstance().getScheduler().schedule(plugin, () -> {
             if (serverInfo.getPlayers().isEmpty()) {
-                ProxyServer.getInstance().getServers().put(serverInfo.getName(), serverInfo);
+                ProxyServer.getInstance().getServers().put(gameServer.getName(), gameServer.getServerInfo());
+                plugin.getLogger().info(String.format("Server %s is synchronized on proxy", gameServer.getName()));
                 scheduledTask.cancel();
             }
         }, 1L, 3L, TimeUnit.SECONDS);
