74a2b1e7a356def74b7300219807f88cf691287f
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-07-08 02:30:58 +0300

    Added constructor with configuration class

--- src/main/java/net/quazar/mg/database/DatabaseConfiguration.java
+++ src/main/java/net/quazar/mg/database/DatabaseConfiguration.java
@@ -1,14 +1,25 @@
 package net.quazar.mg.database;
 
 import lombok.Data;
+import net.md_5.bungee.config.Configuration;
+import net.quazar.mg.PluginConfiguration;
 
 @Data
-public class DatabaseConfiguration {
+public final class DatabaseConfiguration {
 
     private final String host;
     private final int port;
     private final String databaseName;
     private final String user;
     private final String password;
+
+    public DatabaseConfiguration(PluginConfiguration configuration) {
+        Configuration section = configuration.getConfiguration().getSection("database");
+        this.host = section.getString("host");
+        this.port = section.getInt("port");
+        this.databaseName = section.getString("db");
+        this.user = section.getString("user");
+        this.password = section.getString("password");
+    }
 
 }
