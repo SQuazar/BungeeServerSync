1ed80eaafa189b46a52e5f34333f7552dc230480
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-07-09 03:15:51 +0300

    Some changes

--- src/main/java/net/quazar/bsync/database/Sql.java
+++ src/main/java/net/quazar/bsync/database/Sql.java
@@ -1,15 +1,15 @@
-package net.quazar.mg.database;
+package net.quazar.bsync.database;
 
+import net.quazar.bsync.model.ServerType;
 import org.intellij.lang.annotations.Language;
 
 public enum Sql {
-    CREATE_TABLE("CREATE TABLE IF NOT EXISTS `server_info`(\n" +
-            "    `name` VARCHAR(32) NOT NULL PRIMARY KEY,\n" +
-            "    `address` VARCHAR(100) NOT NULL,\n" +
-            "    `motd` VARCHAR(255) NOT NULL,\n" +
-            "    `restricted` BOOLEAN NOT NULL DEFAULT false,\n" +
-            "    `type` VARCHAR(32) DEFAULT 'game'\n" +
-            ");"),
+    CREATE_TABLE("CREATE TABLE IF NOT EXISTS `server_info`(" +
+            "`name` VARCHAR(32) NOT NULL PRIMARY KEY," +
+            "`address` VARCHAR(100) NOT NULL," +
+            "`motd` VARCHAR(255) NOT NULL," +
+            "`restricted` BOOLEAN NOT NULL DEFAULT false," +
+            "`type` VARCHAR(32) DEFAULT '" + ServerType.GAME.name().toLowerCase() + "');"),
     SELECT_BY_NAME("SELECT * FROM `server_info` where `name`=?"),
     SELECT_ALL("SELECT * FROM `server_info`"),
     INSERT("INSERT INTO `server_info`(`name`, `address`, `motd`, `restricted`, `type`) VALUES (?, ?, ?, ?, ?)" +
@@ -18,7 +18,7 @@ public enum Sql {
 
     private final String query;
 
-    Sql(@Language("MySQL") String query) {
+    Sql(@Language("sql") String query) {
         this.query = query;
     }
 
--- src/main/java/net/quazar/bsync/scheduler/DeleteEmptyGameServerTask.java
+++ src/main/java/net/quazar/bsync/scheduler/DeleteEmptyGameServerTask.java
@@ -1,30 +1,29 @@
-package net.quazar.mg.scheduler;
+package net.quazar.bsync.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
-import net.quazar.mg.BungeeServerSync;
-import net.quazar.mg.model.GameServer;
+import net.quazar.bsync.BungeeServerSync;
 
 import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
-public final class EmptyServerInfoTask implements Runnable {
+public class DeleteEmptyGameServerTask implements Runnable {
 
     private final BungeeServerSync plugin;
     private final ServerInfo serverInfo;
-    private final GameServer gameServer;
     private ScheduledTask scheduledTask;
 
     @Override
     public void run() {
         scheduledTask = ProxyServer.getInstance().getScheduler().schedule(plugin, () -> {
             if (serverInfo.getPlayers().isEmpty()) {
-                ProxyServer.getInstance().getServers().put(gameServer.getName(), gameServer.getServerInfo());
-                plugin.getLogger().info(String.format("Server %s is synchronized on proxy", gameServer.getName()));
+//                ProxyServer.getInstance().getServers().put(gameServer.getName(), gameServer.getServerInfo());
+                ProxyServer.getInstance().getConfig().removeServer(serverInfo);
+                plugin.getLogger().info(String.format("Empty server %s is deleted on proxy by scheduled task", serverInfo.getName()));
                 scheduledTask.cancel();
             }
-        }, 1L, 3L, TimeUnit.SECONDS);
+        }, 0L, 3L, TimeUnit.SECONDS);
     }
 }
